11ddf680ffabe8c6e8ea2dd7f5cf128407098322
Edgar Rostro EIRM <eirm@odoo.com>
2023-07-24 17:09:38 -0600

    add ge02_team03

---
+++ ge02_team03/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name' : 'ge02_team03',
+    'summary' : """Module to visualize registered motorcycles on a map""",
+    'description' : """Module to visualize registered motorcycles on a map""",
+    'license' : 'OPL-1',
+    'author' : 'Kawiil-Motors',
+    'website' : 'https://levm-odoo-team3-kawiil-motors-ge2-9076373.dev.odoo.com/',
+    'category' : 'Kauil/GE02',
+    'depends' : ['motorcycle_registry'],
+    'data' : [
+        'views/ge02_team03_menuitems.xml',
+        'views/ge02_team03_views.xml',
+    ],
+    'demo' : [
+
+    ],
+    'application' : True,
+}
