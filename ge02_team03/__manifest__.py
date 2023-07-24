cc4693a5b70a0eef995e083a5e7727090e96550d
Edgar Rostro EIRM <eirm@odoo.com>
2023-07-24 17:09:38 -0600

    implement view on the existing module motorcycle registry
    Fixed bug causing errors when installing module
    
    Made module technical rather than full fledged app
    
    Edited manifest = signs
    
    Made module app to install for testing
    
    changes requestes-added a newline on view file
    
    update .gitignore
    
    delete settings.js
    
    Update __manifest__.py

---
+++ ge02_team03/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name' : 'ge02_team03',
+    'summary' : """Module to visualize registered motorcycles on a map""",
+    'description' : """Module to visualize registered motorcycles on a map""",
+    'license' : 'OPL-1',
+    'author' : 'Kawiil-Motors',
+    'website' : 'https://levm-odoo-team3-kawiil-motors-ge2-9076373.dev.odoo.com/',
+    'version': "1.0.0",
+    'category' : 'Kauil/GE02',
+    'depends' : ['motorcycle_registry'],
+    'data' : [
+        'views/ge02_team03_menuitems.xml',
+        'views/ge02_team03_views.xml',
+    ],
+    'application' : False,
+}
