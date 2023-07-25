693b99cec5d93917c0553a512e35d91fd96a0b19
Ian Seidman <iase@odoo.com>
2023-07-25 09:18:24 -0600

    Pull request corrections

--- ge01_team03/__manifest__.py
+++ ge01_team03/__manifest__.py
@@ -1,32 +1,19 @@
 {
     "name": "Motorcycle Filter",
-    
     "summary": "Manage Registration of Motorcycles",
-    
     "description": """
     Motorcycle Filter
-
 This Module is used to automatically filter products to show only products of motorcycle type.
     """,
-    
-    "version": "0.1",
-    
+    "version": "1.0.0",
     "category": "Kauil/Filters",
-    
     "license": "OPL-1",
-    
-    "depends": ["base","stock", "website", "sale","motorcycle_registry"],
-    
+    "depends": ["sale","motorcycle_registry"],
     "data": ['views/product_template_inherit.xml',
     ],
-    
     "demo": [
     ],
-    
     "author": "kauil-motors",
-    
     "website": "www.odoo.com",
-    
     "application": False,
-    
 }
