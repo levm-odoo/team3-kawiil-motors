2d37bb359ecbd50ecf05a89d2c2694e9f7e17d13
ksgv-odoo <ksgv@odoo.com>
2023-07-25 12:48:46 -0600

    [REM] TE03: Add Models and Fields

--- ge03_team3/__manifest__.py
+++ ge03_team3/__manifest__.py
@@ -1,35 +1,18 @@
 {
     "name": "ge03_team3",
-    
     "summary": "ge03_team3",
-    
     "description": """
-    tasks GE03
-    K'awiil Motors names all of their motorcycles based on the make, model, and year. They don't want to do this for every motorcycle they create, and want it to be computed automatically. But they do not want this to happen on other non-motorcycle products they sell. 
-
-    Specifications 
-    Set the "name" field on product.template to be computed in the format of "year make model" for all motorcycle products. 
-    Suggested Steps/Hints
-    Brush up on computed fields. Make sure to set the correct dependencies on api.depends. 
-    """,
-    
-    "version": "0.1",
-    
+        tasks GE03
+        K'awiil Motors names all of their motorcycles based on the make, model, and year. They don't want to do this for every motorcycle they create, and want it to be computed automatically. But they do not want this to happen on other non-motorcycle products they sell. 
+        Specifications 
+        Set the "name" field on product.template to be computed in the format of "year make model" for all motorcycle products. 
+        Suggested Steps/Hints
+        Brush up on computed fields. Make sure to set the correct dependencies on api.depends.""",
+    "version": "1.0.0",
     "category": "Kauil/Registry",
-    
     "license": "OPL-1",
-    
     "depends": ["motorcycle_registry",],
-    
-    "data": [
-    ],
-    
-    "demo": [],
-    
     "author": "kauil-motors",
-    
     "website": "www.odoo.com",
-    
     "application": False,
-    
 }
