e713a69bf312270f610d879199b7f490fa578ec3
Ian Seidman <iase@odoo.com>
2023-07-26 10:06:54 -0600

    Finished basic sequencing and VIN concatenation

--- ge06_team03/models/manufacturing.py
+++ ge06_team03/models/manufacturing.py
@@ -3,11 +3,31 @@ from odoo import api, fields, models
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
     
-    def action_generate_serial(self):
-        self.ensure_one()
-        self.lot_producing_id = self.env['stock.lot'].create(self._prepare_stock_lot_values())
-        if self.product_id.tracking == 'serial':
-            self.lot_producing_id['name']+"Test"
-            print(self.lot_producing_id['name'])
-            self._set_qty_producing()
+    def action_confirm(self):
+        self._check_company()
+        for production in self:
+            if production.bom_id:
+                production.consumption = production.bom_id.consumption
+            # In case of Serial number tracking, force the UoM to the UoM of product
+            if production.product_tracking == 'serial' and production.product_uom_id != production.product_id.uom_id:
+                production.write({
+                    'product_qty': production.product_uom_id._compute_quantity(production.product_qty, production.product_id.uom_id),
+                    'product_uom_id': production.product_id.uom_id
+                })
+                for move_finish in production.move_finished_ids.filtered(lambda m: m.product_id == production.product_id):
+                    move_finish.write({
+                        'product_uom_qty': move_finish.product_uom._compute_quantity(move_finish.product_uom_qty, move_finish.product_id.uom_id),
+                        'product_uom': move_finish.product_id.uom_id
+                    })
+            production.move_raw_ids._adjust_procure_method()
+            (production.move_raw_ids | production.move_finished_ids)._action_confirm(merge=False)
+            production.workorder_ids._action_confirm()
+        # run scheduler for moves forecasted to not have enough in stock
+        self.move_raw_ids._trigger_scheduler()
+        self.picking_ids.filtered(
+            lambda p: p.state not in ['cancel', 'done']).action_confirm()
+        # Force confirm state only for draft production not for more advanced state like
+        # 'progress' (in case of backorders with some qty_producing)
+        self.filtered(lambda mo: mo.state == 'draft').state = 'confirmed'
+        return True
     
